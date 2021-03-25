6c3c2cc4c41a3b9ada766aeb13cfba6c7481cf5b
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-03-24 21:42:46 -0400

    saving before creating new branch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 //import Apollo server
 const { ApolloServer } = require('apollo-server-express');
@@ -24,6 +26,17 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//serve up static assets: check to see if Node environment is in production.  If it is,
+//we instruct the Express.js server to serve any files in the React application's build dir.
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+// a wildcard GET route for the server.  If we make a GET request to any location on the server
+// that doesnt have an explicit route defined, respond with the production ready React front end
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
