2468ec321c604dea42183ecb616a3ad0145f84d8
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-03-24 23:21:25 -0400

    created navigation, new components, applied react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,16 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// importing react-router-dom components and renaming browserrouter to router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//importing page components
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 //with this code we establish a new connection to the GraphQl server using Apollo.  
 const client = new ApolloClient({
   uri: '/graphql'
@@ -21,13 +31,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-    </div>
+      </Router>
     </ApolloProvider>
   );
 }
